4b6f9759326879c554668fe5293037ce2f781465
Renan Souza <renantrsouza@gmail.com>
2022-12-16 22:36:03 -0400

    add: Details route

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { New } from "../screens/New";
 import { Pools } from "../screens/Pools";
 import { Find } from "../screens/Find";
+import { Details } from "../screens/Details";
 
 import { PlusCircle, SoccerBall } from "phosphor-react-native";
 
@@ -52,6 +53,12 @@ export function AppRoutes(){
       <Screen 
         name="find"
         component={Find}
+        options={{tabBarButton: () => null}}
+      />
+
+      <Screen 
+        name="details"
+        component={Details}
         options={{tabBarButton: () => null}}
       />
     </Navigator>
